4febad1b6d12d0cb322fbe61d7ca1ef3bc17aca7
paseri3739 <tael43@icloud.com>
2023-08-24 15:26:29 +0900

    refactor

---
+++ refactor.py
@@ -0,0 +1,104 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+
+class EBookScraper:
+    USER_NAME = os.getenv("USER_NAME")
+    PASSWORD = os.getenv("PASSWORD")
+    INITIAL_PAGE = os.getenv("INITIAL_PAGE")
+    INFORMATION_URL = "https://kinoden.kinokuniya.co.jp/tosyokan.pref.shizuoka/bookdetail/p/"
+
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10)
+
+    @property
+    def current_url(self):
+        return self.driver.current_url
+
+    @property
+    def actions(self):
+        return ActionChains(self.driver)
+
+    def close_driver(self):
+        self.driver.quit()
+
+    def sign_in(self):
+        self.driver.get(self.INITIAL_PAGE)
+        self.driver.set_window_size(1440, 900)
+        self.driver.find_element(By.ID, "usrcardnumber").send_keys(self.USER_NAME)
+        self.driver.find_element(By.ID, "password").click()
+        self.driver.find_element(By.ID, "password").send_keys(self.PASSWORD)
+        self.driver.find_element(By.CSS_SELECTOR, ".exec").click()
+
+        # 現在のタブのハンドルを取得
+        current_handle = self.driver.current_window_handle
+        self.driver.find_element(By.LINK_TEXT, "電子図書館").click()
+        # 新しいタブのハンドルを取得するための待機処理（適切な待機時間を設定すること）
+        time.sleep(2)
+
+        # すべてのタブのハンドルを取得
+        all_handles = self.driver.window_handles
+
+        # 新しいタブのハンドルにスイッチ
+        for handle in all_handles:
+            if handle != current_handle:
+                self.driver.switch_to.window(handle)
+                break
+
+    def get_ebook_type(self) -> str:
+        while True:
+            try:
+                second_xpath = "/html/body/div[2]/div[3]/div/div/div[2]/div[1]/div[1]/div[3]/div[2]/span[1]"
+                break
+            except:
+                time.sleep(3)
+
+        second_div_text = self.driver.find_element(By.XPATH, second_xpath).text
+
+        ebooktype_xpath = None
+        if second_div_text == "NDC:":  # eISBNがないのでXPATHがずれる
+            ebooktype_xpath = "/html/body/div[2]/div[3]/div/div/div[2]/div[1]/div[1]/div[3]/div[4]/span[2]"
+        else:
+            ebooktype_xpath = "/html/body/div[2]/div[3]/div/div/div[2]/div[1]/div[1]/div[3]/div[5]/span[2]"
+
+        format_info_element = self.driver.find_element(By.XPATH, ebooktype_xpath)
+        format_info_text: str = format_info_element.text
+        return format_info_text
+
+    def is_ui_visible(self) -> bool:
+        appbar = self.driver.find_element(By.CSS_SELECTOR, "header.MuiPaper-root:nth-child(1)")
+        is_visible = appbar.is_displayed()
+        return is_visible
+
+    def hide_ui(self):
+        if self.is_ui_visible():
+            self.actions.send_keys(Keys.SPACE)
+
+    def scrape_epub(self):
+        if self.is_ui_visible():
+            self.actions.send_keys(Keys.ARROW_RIGHT)
+
+    def scrape_pdf(self):
+        pass
+
+    def start_scraping(self):
+        self.sign_in()
+        try:
+            while True:
+                if self.INFORMATION_URL in self.current_url:
+                    print(self.get_ebook_type())
+                time.sleep(3)
+        except KeyboardInterrupt:
+            pass
+
+
+if __name__ == "__main__":
+    ebookscraper = EBookScraper()
+    ebookscraper.start_scraping()
+    ebookscraper.close_driver()
--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 # .envファイルを読み込む
 load_dotenv()
@@ -17,6 +19,7 @@ class EBookScraper:
     @classmethod
     def initialize(cls):
         cls.driver = webdriver.Firefox()
+        cls.driver.implicitly_wait(10)
 
     @classmethod
     def close_driver(cls):
@@ -56,7 +59,6 @@ class EBookScraper:
                 time.sleep(3)
 
         second_div_text = cls.driver.find_element(By.XPATH, second_xpath).text
-        print(second_div_text)
 
         ebooktype_xpath = None
         if second_div_text == "NDC:":  # eISBNがないのでXPATHがずれる
@@ -69,8 +71,21 @@ class EBookScraper:
         return format_info_text
 
     @classmethod
+    def is_ui_visible(cls) -> bool:
+        appbar = cls.driver.find_element(By.CSS_SELECTOR, "header.MuiPaper-root:nth-child(1)")
+        is_visible = appbar.is_displayed()
+        return is_visible
+
+    @classmethod
+    def hide_ui(cls):
+        actions = ActionChains(cls.driver)
+        if cls.is_ui_visible():
+            actions.send_keys(Keys.SPACE)
+
+    @classmethod
     def scrape_epub(cls):
-        pass
+        if cls.is_ui_visible():
+            Keys.ARROW_RIGHT
 
     @classmethod
     def scrape_pdf(cls):
@@ -95,10 +110,12 @@ if __name__ == "__main__":
     EBookScraper.close_driver()
 
 
-# epubContent(普通のイメージのページ)
+# epubContent(普通のイメージのページ) 1ページ目のEPUB: 表紙
 # imageContents(表紙とか、HTMLで制御されていない自炊系の電子書籍) -> 形式: PDFになっている
 # screenDiv(画面全体、見開き範囲外まで)
 # class pageContainer(htmlのとき、見開き)
 # スペースバー押下1回でUIが隠れる
 # 書籍情報に形式:EPUBとあるものはHTMLで記述されている -> 形式を検知し、それに応じたスクレイピングを行う
 # 形式: PDF 普通にスクレイピング可能
+# idc0_348
+# EPUBの場合is_visible = element.is_displayed()でUI要素を消したことを確認してからページ全体でスクリーンショットを取るのがベスト

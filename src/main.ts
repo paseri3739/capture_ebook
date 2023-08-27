291c10585b3588b016e7952303f9417b6cd3c461
paseri3739 <tael43@icloud.com>
2023-08-27 10:06:48 +0900

    file moved

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,17 @@ dotenvConfig();
     const { browser, page } = await initialize(initialUrl);
     const newPage = await login(page, browser);
 
+    // ページ遷移を監視
+    let currentUrl = await newPage.url();
+    newPage.on('framenavigated', async frame => {
+        const newUrl = frame.url();
+        if (newUrl !== currentUrl) {
+            console.log(`URL has changed to: ${newUrl}`);
+            currentUrl = newUrl;
+            // 新しいURLで何らかの処理を行う場合はここに記述
+        }
+    });
+
     // readline インターフェースを一度だけ作成
     const readl = readlineModule.createInterface({
         input: process.stdin,

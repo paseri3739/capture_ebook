d23c3e68dc66ab6e5e8d2e483b14bb97464b443c
paseri3739 <tael43@icloud.com>
2023-12-19 18:54:35 +0900

    remove

--- puppeteer/switch_to_last_tab.ts
+++
@@ -1,7 +0,0 @@
-import { Browser, Page } from 'puppeteer';
-
-export const switchToLastTab = async (browser: Browser): Promise<Page> => {
-    const pages = await browser.pages();
-    const lastPage = pages[pages.length - 1];
-    return lastPage;
-};
